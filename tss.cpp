99ddab0e757bede598cecc8e487a58d46b5ef2a2
unknown <vy6490134@gmail.com>
2024-08-10 23:55:45 +0530

    init

---
+++ tss.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 14;
+    int b = 15;
+    cout<<"This is a tutorial 4. Here is the value of a is "<<a<<". The value of b is "<< b;
+    return 0;
+}
---
+++ tut3.cpp
@@ -0,0 +1,12 @@
+# include<iostream>
+
+using  namespace std;
+
+int main(){
+    int a =4;
+    int b =5;
+    cout<<"This is tutorial 4. Here the value of a is "<<a<<"". The value 
+    of b is "<< b;
+    return 0;
+
+}
